2e579386680093d8b84fb3cb55c7872bc1b1e5e4
jyotijoey <jburnwal5@gmail.com>
2021-06-06 23:40:29 +0530

    updated input file

--- src/Input.js
+++ src/Input.js
@@ -1,15 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./Input.css";
 import { Link } from "react-router-dom";
 
 function Entry() {
+    const [input, setInput] = useState({});
+
     return (
         <div className="input">
-            
+            <input type="text" name="accountName" id="accountName" placeholder="Enter your account name" />
+            <input type="text" name="videoDescription" id="videoDescription" placeholder="Enter account description" />
             <input type="url" name="videoLink" id="videoLink" placeholder="Enter Video Link"/>
             <input type="url" name="song" id="song" placeholder="Enter the Song"/>
             <Link to="/">
-            <button>Save</button>
+            <button onClick="handle new video input">Save</button>
             </Link>
             <Link to="/">
             <button >Cancel</button>
