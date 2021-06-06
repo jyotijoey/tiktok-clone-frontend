7bc1d696cb4a7b884ed4f25dcfa061ee39ac1e26
jyotijoey <jburnwal5@gmail.com>
2021-06-07 03:06:20 +0530

    attached input.js to backend

--- src/Input.js
+++ src/Input.js
@@ -1,22 +1,25 @@
-import React, { useState } from 'react';
+import React from 'react';
 import "./Input.css";
 import { Link } from "react-router-dom";
 
 function Entry() {
-    const [input, setInput] = useState({});
 
     return (
         <div className="input">
-            <input type="text" name="accountName" id="accountName" placeholder="Enter your account name" />
-            <input type="text" name="videoDescription" id="videoDescription" placeholder="Enter account description" />
-            <input type="url" name="videoLink" id="videoLink" placeholder="Enter Video Link"/>
-            <input type="url" name="song" id="song" placeholder="Enter the Song"/>
-            <Link to="/">
-            <button onClick="handle new video input">Save</button>
-            </Link>
+        <form action="http://localhost:9000/v2/posts" method="post" onSubmit={()=> this.history.push("/")}>
+        
+            <input type="text" name="channel" placeholder="Enter your account name" />
+            <input type="text" name="description" placeholder="Enter account description" />
+            <input type="text" name="link" placeholder="Enter Video Link"/>
+            <input type="text" name="song" placeholder="Enter the Song"/>
+            
+            {/* <Link to="/"> */}
+            <button type="submit" >Save</button>
+            {/* </Link> */}
             <Link to="/">
             <button >Cancel</button>
             </Link>
+        </form>
         </div>
     )
 }
